c9472a030b67f45b6f23f1dbffbceb271575c288
a1147409 <a1147409@unet.univie.ac.at>
2016-12-12 14:20:29 +0100

    TODO-LIST updated

--- BESTFitness/src/modells/statistics.java
+++ BESTFitness/src/modells/statistics.java
@@ -1,6 +1,35 @@
 package modells;
 
 public class statistics {
-	//test
+	public statistics(){}
+	
+	
+	public void topRatedCourse(){
+		//TODO: 1. Top bewerteste Kurse
+	}
+	
+	public void currentlyCourses(){
+		//TODO: 2. Aktuelle Kurse
+	}
+	
+	public void completedCourses(){
+		//TODO: 3. Abgeschlossene Kurse
+	}
+	
+	public void mostVisitedCourses(){
+		//TODO: 4. Meistbesuchte Kurse
+	}
+	
+	public void mostClickedTrainers(){
+		//TODO: 5. Meistbesuchte Trainer
+	}
+	
+	public void courseMembers(){
+		//TODO: 6. Aktuelle Kurse Kurse mit Teilnehmer
+	}
+	
+	public void completedCoursesMembers(){
+		//TODO: 7. Abgeschlossene Kurse mit Teilnehmer
+	}
 
 }
