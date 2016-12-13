2b90ec3ddf766218eb90c5af17fcf61df8554410
a1147409 <a1147409@unet.univie.ac.at>
2016-12-13 03:37:03 +0100

    bisschen logik eingefügt

--- BESTFitness/src/modells/statistics.java
+++ BESTFitness/src/modells/statistics.java
@@ -1,19 +1,43 @@
 package modells;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Daos.KursManagement;
+import modells.Kurs;
+@SuppressWarnings("unused")
 public class statistics {
+	private Object kurse;
+
 	public statistics(){}
 	
 	
 	public void topRatedCourse(){
 		//TODO: 1. Top bewerteste Kurse
+		KursManagement kurse = new KursManagement("kurs.txt");
+		int anzahl = 0;
+		for(Kurs kurs: kurse.getKursList()){
+			
+		}
 	}
 	
-	public void currentlyCourses(){
+	public int currentlyCourses(){
 		//TODO: 2. Aktuelle Kurse
+		KursManagement kurse = new KursManagement("kurs.txt");
+		int anzahl = kurse.getKursList().size();
+		return anzahl;
 	}
 	
 	public void completedCourses(){
 		//TODO: 3. Abgeschlossene Kurse
+		KursManagement kurse = new KursManagement("completed.txt");
+		List<String> completedCourses = new ArrayList<String>();
+	
+		for(Kurs kurs: kurse.getKursList()){
+			if(true){ //TODO: Prüfe ob Kurs schon abgeschlossen ist.
+				completedCourses.add((String) this.kurse);
+			}
+		}	
 	}
 	
 	public void mostVisitedCourses(){
@@ -21,15 +45,15 @@ public class statistics {
 	}
 	
 	public void mostClickedTrainers(){
-		//TODO: 5. Meistbesuchte Trainer
+		//TODO: 5. Meistbesuchte Trainer (LeiterView)
 	}
 	
 	public void courseMembers(){
-		//TODO: 6. Aktuelle Kurse Kurse mit Teilnehmer
+		//TODO: 6. Aktuelle Kurse Kurse mit Teilnehmer (TrainerView)
 	}
 	
 	public void completedCoursesMembers(){
-		//TODO: 7. Abgeschlossene Kurse mit Teilnehmer
+		//TODO: 7. Abgeschlossene Kurse mit Teilnehmer (TrainerView)
 	}
 
 }
