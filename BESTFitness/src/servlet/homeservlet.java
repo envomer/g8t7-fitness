45289e62236ccc5ce8c66cdd23931396e81acc29
r.abit <r.abit@eduroam-026-017.wlan.univie.ac.at>
2017-01-29 12:35:04 +0100

    added - homeServlet

---
+++ BESTFitness/src/servlet/homeservlet.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import raum.Raum;
+import raum.RaumManagement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class homeservlet extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        request.getRequestDispatcher("home.jsp").include(request, response);
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        response.sendRedirect("/raum");
+    }
+}
