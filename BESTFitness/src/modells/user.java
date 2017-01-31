5f6c85d655402e0ac9429177df6883c36df51663
r.abit <r.abit@eduroam-027-176.wlan.univie.ac.at>
2017-01-31 20:55:49 +0100

    Javadoc in user.java - added

--- BESTFitness/src/modells/user.java
+++ BESTFitness/src/modells/user.java
@@ -16,6 +16,15 @@ public class user implements Serializable{
 	private String pass;
 
 
+	/**
+	 * Benutzer Objekte wertden mit Werten erstellt
+	 * @param benutzertyp
+	 * @param benutzerid
+	 * @param benutzernachname
+	 * @param benutzervorname
+	 * @param addresse
+	 * @param pass
+	 */
 	public user(String benutzertyp, String benutzerid, String benutzernachname, String benutzervorname, String addresse, String pass) {
 		this.benutzertyp = benutzertyp;
 		this.benutzerid=benutzerid;
@@ -27,58 +36,114 @@ public class user implements Serializable{
 	}
 
 
-
+	/**
+	 * getBenutzertypeClean
+	 * @return
+	 */
 	public String getBenutzertypeClean() {
         return this.benutzertyp.replace(".jsp", "");
     }
+	
+	/**
+	 * getBenutzertyp
+	 * @return
+	 */
 	public String getBenutzertyp() {
 		return this.benutzertyp;
 	}
 
+	/**
+	 * setBenutzertyp
+	 * @param benutzertyp
+	 */
 	public void setBenutzertyp(String benutzertyp) {
 		 this.benutzertyp = benutzertyp;
 	}
 
+	/**
+	 * getBenutzerid
+	 * @return
+	 */
 	public String getBenutzerid() {
 		return benutzerid;
 	}
 
+	/**
+	 * setBenutzerid
+	 * @param benutzerid
+	 */
 	public void setBenutzerid(String benutzerid) {
 		this.benutzerid = benutzerid;
 	}
 
+	/**
+	 * getBenutzernachname
+	 * @return
+	 */
 	public String getBenutzernachname() {
 		return benutzernachname;
 	}
 
+	/**
+	 * setBenutzernachname
+	 * @param benutzernachname
+	 */
 	public void setBenutzernachname(String benutzernachname) {
 		this.benutzernachname = benutzernachname;
 	}
 
+	/**
+	 * getBenutzervorname
+	 * @return
+	 */
 	public String getBenutzervorname() {
 		return benutzervorname;
 	}
 
+	/**
+	 * setBenutzervorname
+	 * @param benutzervorname
+	 */
 	public void setBenutzervorname(String benutzervorname) {
 		this.benutzervorname = benutzervorname;
 	}
 
+	/**
+	 * getAddresse
+	 * @return
+	 */
 	public String getAddresse() {
 		return addresse;
 	}
 
+	/**
+	 * setAddresse
+	 * @param addresse
+	 */
 	public void setAddresse(String addresse) {
 		this.addresse = addresse;
 	}
 
+	/**
+	 * getPass
+	 * @return
+	 */
 	public String getPass() {
 		return pass;
 	}
 
+	/**
+	 * setPass
+	 * @param pass
+	 */
 	public void setPass(String pass) {
 		this.pass = pass;
 	}
 
+	/**
+	 * getName
+	 * @return
+	 */
 	public String getName() {
         return this.benutzervorname + " " + this.benutzernachname;
     }
