07371d6d836b27465040feaa8ee8e6ebdf44fca7
r.abit <r.abit@192.168.1.2>
2017-01-31 22:59:33 +0100

    javadoc small fixes in KursDao.java

--- BESTFitness/src/Daos/KursDao.java
+++ BESTFitness/src/Daos/KursDao.java
@@ -21,20 +21,22 @@ public interface KursDao {
 	public Kurs getKursById(int id);
 
 	/**
-	 * speichert Objekt kurs in die Datei
-	 * @param kurs	Objekt "k" gespeichert werden soll
+	 * speichert Objekt kurs (k) in die Datei
+	 * @param
 	 */
 	public void speicherKurs(Kurs k);
 	
 	/**
 	 * löscht den Kurs falls enthaelt
-	 * @param "k" das Objekt des Kurses die gelöscht werden soll
+	 * "k" das Objekt des Kurses die gelöscht werden soll
+	 * @param 
 	 */
 	public void loescheKurs(Kurs k);
 	
 	/**
 	 * Der Kurs mit der ID des Kurs aus der Liste gefunden und gelöscht dann eine neue Kurs einfügen werden soll
-	 * @param k zu veraendernder Kurs
+	 * k zu veraendernder Kurs
+	 * @param 
 	 */
 	public void aenderKurs(Kurs k);
 	
